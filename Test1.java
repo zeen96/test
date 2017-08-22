5b8863a1981fa8315d5d677a684eae3a6b2ae57b
zeen96 <zeenying@163.com>
2017-08-22 19:48:36 +0800

    Upload a file
    upload

---
+++ Test1.java
@@ -0,0 +1,27 @@
+class Test1 {
+	public static void main(String[] args) {
+		System.out.println("hello yze!");
+		float f;
+		int a = 3;
+		f = a;
+		System.out.println(f);
+		System.out.println(5/5/5*5*5);
+		f = 0;
+		f = 5/5/5*5*5;
+		System.out.println(f);
+		f = 5.0f/5/5*5*5;
+		System.out.println(f);
+		
+		int i = 5;
+		switch(i) {
+			case 5:
+				System.out.println("5");
+			default:
+				System.out.println("default");
+				break;
+			case 3:
+				System.out.println("3");
+				break;
+		}
+	}
+}
